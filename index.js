13139e0136fb6e2de34d7d358c7c5e2fda14abe0
henry <henry.git@outlook.com>
2021-10-28 23:58:56 +0800

    :sparkles: 集成release-drafter

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-// const releaseDrafter = require("release-drafter-github-app");
-
 module.exports = (app) => {
     app.log.info("Yay, the app was loaded!");
-    // releaseDrafter(app,{})
 
-    // app.on("milestone.closed", async (context) => {
-    //     await releaseDrafter.apply()
-    //     const issueComment = context.issue({
-    //         body: "Thanks for opening this issue!",
-    //     });
-    //     return context.octokit.issues.createComment(issueComment);
-    // });
+    app.on("milestone.closed", async (context) => {
+        // TODO 自动发布release,创建下一个里程碑
+        app.log.info("release.published");
+    });
 
-    // For more information on building apps:
-    // https://probot.github.io/docs/
-
-    // To get your app running against GitHub, see:
-    // https://probot.github.io/docs/development/
 };
