32bffaac4afd10309025acaff4ddf2e24828973a
henry <henry.git@outlook.com>
2021-10-30 00:44:46 +0800

    ✨ 里程碑关闭自动创建下一个里程碑

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # bot
 
-IHub Bot
+IHub自动化工具应用集，组合应用如下：
+
+- [Release Drafter](https://github.com/marketplace/actions/release-drafter)
+- [PR Milestone Check](https://github.com/marketplace/pr-milestone-check)
--- index.js
+++ index.js
@@ -1,31 +1,18 @@
-const {createPullRequestStatus} = require("./lib/check-milestone")
+const {checkMilestone} = require("./lib/check-milestone")
+const {getVersionInfo} = require('release-drafter-github-app/lib/versions')
 
 module.exports = app => {
-    app.log.info("Yay, the app was loaded!");
+    app.log.info("Yay, the app was loaded!")
+
+    checkMilestone(app)
 
     app.on("milestone.closed", async context => {
-        // TODO 自动发布release,创建下一个里程碑
-        app.log.info("release.published");
+        // 自动创建下一个里程碑
+        await context.octokit.rest.issues.createMilestone({
+            owner: context.payload.repository.owner.login,
+            repo: context.payload.repository.name,
+            title: getVersionInfo(context.payload.milestone.title, null).$RESOLVED_VERSION.version
+        })
     });
-
-    app.on("pull_request", async context => {
-        app.log.info("Received pull_request webhook")
-        createPullRequestStatus(context, context.payload.pull_request)
-    })
-
-    app.on("issues", async context => {
-        app.log.info("Received issue webhook")
-        if (context.payload.issue.pull_request != null) {
-            app.log.info("Issue is a PR")
-            const pr = await context.octokit.pulls.get({
-                owner: context.payload.repository.owner.login,
-                repo: context.payload.repository.name,
-                pull_number: context.payload.issue.number
-            })
-            createPullRequestStatus(context, pr.data)
-        } else {
-            app.log.info("Issue is not a PR!")
-        }
-    })
 
 }
