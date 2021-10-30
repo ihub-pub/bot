a2874aef423744efb432c8e0d9fa3f3da1eda066
henry <henry.git@outlook.com>
2021-10-30 11:27:00 +0800

    ✨ 发布release自动关闭里程碑

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const {checkMilestone} = require("./lib/check-milestone")
-const {getVersionInfo} = require('release-drafter-github-app/lib/versions')
+const {autoMilestone} = require("./lib/auto-milestone")
 
 module.exports = app => {
     app.log.info("Yay, the app was loaded!")
 
     checkMilestone(app)
 
-    app.on("milestone.closed", async context => {
-        // 自动创建下一个里程碑
-        await context.octokit.rest.issues.createMilestone({
-            owner: context.payload.repository.owner.login,
-            repo: context.payload.repository.name,
-            title: getVersionInfo(context.payload.milestone.title, null).$RESOLVED_VERSION.version
-        })
-    });
+    autoMilestone(app)
 
     app.on("workflow_run.completed", async context => {
         const run = context.payload.workflow_run
