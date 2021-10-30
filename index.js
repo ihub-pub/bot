5b6f474bbee90a67787030c205cc0e86352adc56
henry <henry.git@outlook.com>
2021-10-30 09:36:12 +0800

    ✨ 添加定时任务工作流任务重试

--- index.js
+++ index.js
@@ -15,4 +15,17 @@ module.exports = app => {
         })
     });
 
+    app.on("workflow_run.completed", async context => {
+        const run = context.payload.workflow_run
+        // 定时任务工作流失败时重试，最多重试2次
+        if (run.event === "schedule" && run.conclusion === "failure" && run.run_attempt < 3) {
+            app.log.info("Rerun Workflow Run")
+            await context.octokit.request('POST /repos/{owner}/{repo}/actions/runs/{run_id}/rerun', {
+                owner: context.payload.repository.owner.login,
+                repo: context.payload.repository.name,
+                run_id: run.id
+            })
+        }
+    });
+
 }
