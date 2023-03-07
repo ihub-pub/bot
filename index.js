30fd9f01f9f58207d972de0e896d0decf8f5125a
Henry <henry.git@outlook.com>
2023-03-07 09:14:56 +0800

    Update index.js
    Signed-off-by: Henry <henry.git@outlook.com>

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ module.exports = app => {
 
     app.on("workflow_run.completed", async context => {
         const run = context.payload.workflow_run
-        // 定时任务工作流失败时重试，最多重试4次
-        if (run.event === "schedule" && run.conclusion === "failure" && run.run_attempt < 5) {
+        // 定时任务或者发布任务工作流失败时重试，最多重试4次
+        if ((run.event === "schedule" ||run.event === "release")  && run.conclusion === "failure" && run.run_attempt < 5) {
             app.log.info("Rerun Workflow Run")
             await context.octokit.request('POST /repos/{owner}/{repo}/actions/runs/{run_id}/rerun', {
                 owner: context.payload.repository.owner.login,
