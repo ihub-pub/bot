2f4bf94bf1a8e74dbcfa91331c077b250c429681
henry <henry.git@outlook.com>
2023-04-17 16:36:15 +0800

    feat: 改用vercel部署bot服务

--- app.js
+++ app.js
@@ -24,4 +24,10 @@ module.exports = app => {
         }
     });
 
+    app.on("issues.opened", async (context) => {
+        return context.octokit.issues.createComment(
+            context.issue({ body: "Hello, World!" })
+        );
+    });
+
 }
